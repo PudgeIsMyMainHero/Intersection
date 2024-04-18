901d221f7e92a8b04c8ff7d1ea773d81e3b1920d
Wattelon <Mr.Smayl546@gmail.com>
2024-04-18 19:05:37 +0300

    Baker's day

---
+++ Assets/Scripts/YellowGroup.cs
@@ -0,0 +1,8 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class YellowGroup : MonoBehaviour
+{
+   public readonly Collider[] yellowMoved = new Collider[3];
+}
--- Assets/YellowGroup.cs
+++
@@ -1,8 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class YellowGroup : MonoBehaviour
-{
-   public readonly Collider[] yellowMoved = new Collider[3];
-}
